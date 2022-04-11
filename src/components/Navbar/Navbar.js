c55d36e22822066eaa504c5c99e44d2d1670194c
MD Limon Farazi <abidislamabir@gmail.com>
2022-04-11 21:40:00 +0600

    some change

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import logo from '../Usefull/Images/logo.png'
 
 const Navbar = () => {
   return (
     <div>
-      <h1>this is navbar</h1>
+      <img width='150px' src={logo} alt="" />
+      <ul>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+      </ul>
     </div>
   );
 };
